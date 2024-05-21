cb6e34cd2538cfb5b60f2d555696b0c9addc7987
Laurenda Edorh <laurenda98@gmail.com>
2024-05-21 21:57:43 +0200

    creation de controler

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/etudiant', function () {
+    $test = "yann" . "banvi";
+    return view("etudiant", compact("test"));
+});
