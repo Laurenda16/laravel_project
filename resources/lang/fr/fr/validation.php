830ec87b05b488dfab47e173080aa8c3b041ff3f
Laurenda Edorh <laurenda98@gmail.com>
2024-05-26 18:33:02 +0200

    afficher message d'erreur

---
+++ resources/lang/fr/fr/validation.php
@@ -0,0 +1,15 @@
+<?php
+/*return [
+    'custom' => [
+        'nom' => [
+            'required' => 'Le champs nom est obligatoire'
+        ],
+        'prenom' => [
+            'required' => 'Le champs prenom est obligatoire'
+        ],
+
+        'classe_id' => [
+            'required' => 'Le champs classe doit est obligatoire!'],
+
+    ],
+];*/
--- resources/views/createEtudiant.blade.php
+++ resources/views/createEtudiant.blade.php
@@ -9,6 +9,14 @@
       <h3 class="border-bottom pb-2 mb-4">Ajout d'un nouvel etudiant</h3>
   
       <div class="mt-4">
+        @if (session()->has('success'))
+        <div class="alert alert-success">
+          {{session()->get('success')}}
+        </div>
+          
+          
+        @endif
+
         @if ($errors->any())
           
   <div class="alert alert-danger">
@@ -26,18 +34,18 @@
           
             <div class="mb-3">
               <label for="exampleInputEmail1" class="form-label">Nom Etudiant</label>
-              <input type="text" class="form-control" >
+              <input type="text" class="form-control" required name="nom">
            
             </div>
             <div class="mb-3">
               <label for="exampleInputPassword1" class="form-label">Prenom Etudiant</label>
-              <input type="text" class="form-control" >
+              <input type="text" class="form-control" required name="prenom">
             </div>
             <div class="mb-3">
                 <label for="exampleInputPassword1" class="form-label">Classe</label>
 
-               <select class="form-control" >
-                <option value=" "></option>
+               <select class="form-control" name="classe_id">
+                <option value=""></option>
                 @foreach($classes as $classe )
                 <option value="{{ $classe->id}}">{{"$classe->libelle"}} </option>
                 @endforeach
